3539d36c57e2624df150d84703ba2a6485177d41
Patel-Aditya02 <4libaba40@gmail.com>
2026-09-13 02:02:44 +0530

    Added required apis to backend

--- BACKEND/fuzzy_search.py
+++ BACKEND/fuzzy_search.py
@@ -4,14 +4,9 @@ LIMIT = 20
 SCORE_CUTOFF = 60
 
 def search(query: str, sql_data):
-     short_list = process.extract(query, sql_data, limit=LIMIT, score_cutoff=60)
-     return short_list
+     return process.extract(query, sql_data, limit=LIMIT, score_cutoff=60)
 
 
-if __name__ == '__main__':
-     sql_results = ["Apple iPhone 15", "Apple iPhone 14 Pro", "Samsung Galaxy S24", "Google Pixel 8"]
-     user_query = "iphone15"
+def filter():
+     ...
 
-     search_result = search(user_query, sql_results)
-     print(type(sql_results))
-     print(search)
